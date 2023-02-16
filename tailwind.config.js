70d1ab19fff17199601df29bbdec488cccea7cd0
udbern <edwinuduakabasi@gmail.com>
2023-02-16 14:28:09 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'white-wisper':'#ECE7DA',
+        'vanilla':'#F7E0D2'
+      }
+    },
+  },
+  plugins: [],
+}
